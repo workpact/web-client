2e41e8a3398efc70ad9cb4fc6160c3c8c12932c9
speakerwiggin <morgan@shapeshift.io>
2018-02-17 22:15:41 -0700

    progress bar

--- src/js/components/user/presentation/rating.jsx
+++ src/js/components/user/presentation/rating.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Rating = ({ data: averageRating }) => {
-  return <div className='User-rating'>Average rating: {averageRating}</div>
+  return (
+    <div className='User-rating'>
+      Average rating:{' '}
+      <progress className='ProgressBar' max='2' value={averageRating || 0} />
+    </div>
+  )
 }
 
 export default Rating
